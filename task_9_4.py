0e812969d53d9bf15f95f876e25f3c274c4b00c8
PARZHEV <88132441+PARZHEV@users.noreply.github.com>
2021-12-03 17:25:31 +0300

    Add files via upload

---
+++ task_9_4.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 4. Реализуйте базовый класс Car.
+# у класса должны быть следующие атрибуты: speed, color, name, is_police(булево). 
+# А также методы: go, stop, turn(direction), которые должны сообщать, что машина поехала, остановилась, повернула (куда);
+# опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar;
+# добавьте в базовый класс метод show_speed, который должен показывать текущую скорость автомобиля;
+# для классов TownCar и WorkCar переопределите метод show_speed. 
+# При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
+
+
+# In[ ]:
+
+
+class Car:
+    speed
+    color
+    name
+    is_police
+    
+    def go(self):
+    def stop(self):
+    def turn(self):
+
